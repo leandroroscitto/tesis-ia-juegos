cf2105d4be99da35ec9906c08bcad5d990a4007f
leandroroscitto <leandroroscitto@ed1921fa-bea4-e9ab-b628-ee224fede172>
2013-03-05 16:01:40 +0000

    Agregada compresion de los datos almacenados durante la serializacion.

--- Assets/Scripts/Escenario/Generadores/Generador_Mapa.cs
+++ Assets/Scripts/Escenario/Generadores/Generador_Mapa.cs
@@ -136,15 +136,17 @@ public class Generador_Mapa : MonoBehaviour {
 
    // Operaciones
    public void OnDrawGizmosSelected() {
-	  if (conexiones_horizontales != null) {
-		 foreach (Conexion conexion in conexiones_horizontales) {
-			Gizmos.DrawLine(mapa.posicionRepresentacionAReal(new Vector2(conexion.x1, conexion.y1), 1.25f), mapa.posicionRepresentacionAReal(new Vector2(conexion.x2, conexion.y2), 1.25f));
+	  if (mapa != null) {
+		 if (conexiones_horizontales != null) {
+			foreach (Conexion conexion in conexiones_horizontales) {
+			   Gizmos.DrawLine(mapa.posicionRepresentacionAReal(new Vector2(conexion.x1, conexion.y1), 1.25f), mapa.posicionRepresentacionAReal(new Vector2(conexion.x2, conexion.y2), 1.25f));
+			}
 		 }
-	  }
 
-	  if (conexiones_verticales != null) {
-		 foreach (Conexion conexion in conexiones_verticales) {
-			Gizmos.DrawLine(mapa.posicionRepresentacionAReal(new Vector2(conexion.x1, conexion.y1), 1.25f), mapa.posicionRepresentacionAReal(new Vector2(conexion.x2, conexion.y2), 1.25f));
+		 if (conexiones_verticales != null) {
+			foreach (Conexion conexion in conexiones_verticales) {
+			   Gizmos.DrawLine(mapa.posicionRepresentacionAReal(new Vector2(conexion.x1, conexion.y1), 1.25f), mapa.posicionRepresentacionAReal(new Vector2(conexion.x2, conexion.y2), 1.25f));
+			}
 		 }
 	  }
    }
